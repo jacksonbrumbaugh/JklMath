175e7fce0babd91896c7a319035e2da3ef000466
Jackson Brumbaugh <jcb5582@gmail.com>
2022-09-14 16:27:54 -0400

    Set up module heart files (psm1 & psd1)

---
+++ JklMath.psm1
@@ -0,0 +1,41 @@
+<# Metadata
+  CreatedBy : Jackson Brumbaugh
+  CreatedOn : 2022-09-14
+  Version : 20220914-A
+#>
+
+$ModuleHome = $PSScriptRoot
+
+$ModuleDirs = @(
+  @{ Export = $false ; Name = "HelperCmds"   }
+  @{ Export = $true  ; Name = "MainCmds"     }
+  @{ Export = $true  ; Name = "UnderDevCmds" }
+)
+
+$DirsToLoad = $ModuleDirs.Name
+$DirsToExport = $ModuleDirs.where{ $_.Export -eq $true }.Name
+
+foreach ( $Dir in $DirsToLoad ) {
+  $ExportCmds = if ( $Dir -in $DirsToExport ) {
+    $true
+  } else {
+    $false
+  }
+
+  $FullPath = Join-Path $ModuleHome $Dir
+  if ( Test-Path $FullPath ) {
+    $Scripts = Get-ChildItem $FullPath\*ps1
+
+    foreach ( $Script in $Scripts ) {
+      . $Script.FullName
+
+      if ( $ExportCmds ) {
+        Export-ModuleMember -Function $Script.BaseName
+      }
+
+    }
+  } else {
+    Write-Warning "Failed to locate the $Dir directory in the JklScryfall PowerShell module"
+  }
+
+} # End block:foreach Dir in DirsToLoad
